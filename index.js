5a0a5843f12d7ec4ffd6b8a0f7c0530ee03ba6b2
ktotheH <kcjhill1234@msn.com>
2020-06-08 21:54:32 -0600

    wip

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
-app.engine('handlebars', handleBars({defaultLayout: "main"}));
+app.engine('handlebars', handleBars({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
-res.render('index');
+    res.render('index');
 })
 app.listen(PORT, () => {
     console.log("server listening on localhost:" + PORT);
