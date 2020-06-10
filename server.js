7630f75e0978a6ec12c95949f74932f144dbeb59
ktotheH <kcjhill1234@msn.com>
2020-06-09 20:32:05 -0600

    update server; install packages; create db

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const handleBars = require('express-handlebars');
 const path = require('path')
 const PORT = 8080;
 const app = express();
+const optionRoutes = require('./routes/options');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
@@ -11,6 +13,8 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     res.render('index');
 })
+optionRoutes(app);
+
 app.listen(PORT, () => {
-    console.log("server listening on localhost:" + PORT);
+    console.log("server listening on http://localhost:" + PORT);
 })
