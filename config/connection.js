59db65f1fade567fb42ea23feeee857b4a86b947
ktotheH <kcjhill1234@msn.com>
2020-06-14 11:56:06 -0600

    fix mysql

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("kitchenBoss", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize("kitchenBoss", "root", "password", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+  });
+}
+
 module.exports = sequelize;
